9ff6c64405514f939a32492e779422377c16bdf4
nateclos <nclos@email.arizona.edu>
2019-04-10 22:39:15 -0700

    Starting working further on the indexing

--- src/main/java/edu/arizona/cs/Indexer.java
+++ src/main/java/edu/arizona/cs/Indexer.java
@@ -2,30 +2,35 @@ package edu.arizona.cs;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
 
 public class Indexer {
+	
+	private FSDirectory index;
+	private StandardAnalyzer analyzer;
 	
 	public Indexer(String input_file) throws IOException {
 		
 		ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource(input_file).getFile());
         
-        StandardAnalyzer analyzer = new StandardAnalyzer();
-        Directory index = new RAMDirectory();
+        this.analyzer = new StandardAnalyzer();
+        this.index = FSDirectory.open(Paths.get("/Users/nateclos/Documents/cs483/WatsonQASystem/src/main/resources/index.lucene"));
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         IndexWriter w = new IndexWriter(index, config);
-        
+        w.close();
+        index.close();
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		File f = new File(".");
-		System.out.println(f.getAbsolutePath());
+		Indexer i = new Indexer("enwiki-20140602-pages-articles.xml-0005.txt");
 	}
 }
