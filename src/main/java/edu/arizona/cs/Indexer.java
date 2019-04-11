187cf3f68ee236a8941a46097d867057a74bc81e
nateclos <nclos@email.arizona.edu>
2019-04-10 23:14:49 -0700

    Testing out indexing files by printing

--- src/main/java/edu/arizona/cs/Indexer.java
+++ src/main/java/edu/arizona/cs/Indexer.java
@@ -3,6 +3,7 @@ package edu.arizona.cs;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
@@ -20,17 +21,24 @@ public class Indexer {
 		
 		ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource(input_file).getFile());
-        
-        this.analyzer = new StandardAnalyzer();
+        Scanner s = new Scanner(file);
+        int end = 100;
+        while(end != 0) {
+        		String curr = s.next();
+        		System.out.println(curr);
+        		end--;
+        }
+        /*this.analyzer = new StandardAnalyzer();
         this.index = FSDirectory.open(Paths.get("/Users/nateclos/Documents/cs483/WatsonQASystem/src/main/resources/index.lucene"));
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         IndexWriter w = new IndexWriter(index, config);
         w.close();
-        index.close();
+        index.close();*/
 	}
 	
 	public static void main(String[] args) throws IOException {
 		
 		Indexer i = new Indexer("enwiki-20140602-pages-articles.xml-0005.txt");
+		
 	}
 }
